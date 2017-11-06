f0a397418f4e8a7f492214bb57fc400820a8e499
DRAJ <davidrajnus@gmail.com>
2017-11-07 01:15:18 +0800

    css styling and material design applied

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,4 +1,25 @@
 //Check/Uncheck off todo by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  })
+  event.stopPropagation();
+});
+
+$("input").keypress(function(event) {
+  if(event.which === 13){
+    //grabbing new todo text fomr input
+    var todoText = $(this).val();
+    $(this).val("");
+    //append new item to list
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+  }
+});
+
+$(".fa-pencil").click(function() {
+  $("input").fadeToggle();
 });
